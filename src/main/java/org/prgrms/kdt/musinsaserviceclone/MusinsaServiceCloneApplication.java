40d1fd7050e443cecd19b0e5b645e53070cace8e
Kim-AYoung <onionlily123@naver.com>
2022-10-17 16:34:22 +0900

    feat: init spring project

---
+++ src/main/java/org/prgrms/kdt/musinsaserviceclone/MusinsaServiceCloneApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.kdt.musinsaserviceclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaServiceCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaServiceCloneApplication.class, args);
+    }
+
+}
